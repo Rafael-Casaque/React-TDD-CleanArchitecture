06337889e13592ddaa99fdb40ba08a43d0a01a98
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2022-12-21 17:08:59 -0300

    test: ensure login present error if email is invalid

--- src/main/presentation/pages/login/login.spec.tsx
+++ src/main/presentation/pages/login/login.spec.tsx
@@ -9,8 +9,10 @@ type SutTypes = {
     validationSpy: ValidationSpy
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (): SutTypes => {        
     const validationSpy = new ValidationSpy()
+    const error = faker.random.words()
+    validationSpy.errorMessage = error
     const sut = render(<Login validation={validationSpy} />)
     return {
         sut,
@@ -21,13 +23,13 @@ const makeSut = (): SutTypes => {
 describe('Login component', () => {
     afterEach(cleanup)
     test('Should start with initial state', () => {
-        const { sut } = makeSut()
+        const { sut, validationSpy } = makeSut()
         const errorWrap = sut.getByTestId("error-wrap")
         expect(errorWrap.childElementCount).toBe(0)
         const submitButton = sut.getByTestId("submit") as HTMLButtonElement
         expect(submitButton.disabled).toBe(true)
         const emailStatus = sut.getByTestId("email-status")
-        expect(emailStatus.title).toBe("Campo obrigatório")
+        expect(emailStatus.title).toBe(validationSpy.errorMessage)
         expect(emailStatus.textContent).toBe("🔴")
         const passwordStatus = sut.getByTestId("password-status")
         expect(passwordStatus.title).toBe("Campo obrigatório")
@@ -50,5 +52,14 @@ describe('Login component', () => {
         fireEvent.input(passwordInput, { target: { value: password } })
         expect(validationSpy.fieldName).toBe('password')
         expect(validationSpy.fieldValue).toBe(password)
+    })
+
+    test('Should show email error if validation fails', () => {
+        const { sut, validationSpy } = makeSut()
+        const emailInput = sut.getByTestId("email")        
+        fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+        const emailStatus = sut.getByTestId("email-status")
+        expect(emailStatus.title).toBe(validationSpy.errorMessage)
+        expect(emailStatus.textContent).toBe("🔴")
     })
 });
