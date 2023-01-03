5fb355ace8e8457905d2a00e5539afc4a7f4dcd7
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-01-03 14:47:41 -0300

    feat: ensure MinLengthValidation return false if value is valid

--- src/validation/validators/min-length/min-length-validation.ts
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -1,9 +1,10 @@
+import { ValidationStub } from "@/presentation/test";
 import { FieldValidation } from "@/validation/protocols/field-validation";
 import { InvalidFieldError } from "../email/email-validation";
 
 export class MinLengthValidation implements FieldValidation {
     constructor(readonly field: string, private readonly minLength: number){}    
-    validate(value: string): Error {
-        return new InvalidFieldError()
+    validate(value: string): Error {        
+        return value.length > this.minLength ? null : new InvalidFieldError()
     }
 }
