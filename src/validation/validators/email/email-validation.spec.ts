4686092f790da22a7800514659ba43f523e57e16
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-01-03 14:12:57 -0300

    test: ensure EmailValidation return falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import { EmailValidation } from "./email-validation";
 import { InvalidEmailError } from "@/validation/errors"
+import { faker } from "@faker-js/faker";
 
 describe('EmailValidation', () => {
     test('should return error if email is invalid', () => {
-        const sut = new EmailValidation('email')    
-        const error = sut.validate('email')
+        const sut = new EmailValidation(faker.internet.email())    
+        const error = sut.validate(faker.random.word())
         expect(error).toEqual(new InvalidEmailError())
+    });
+
+    test('should return falsy if email is invalid', () => {
+        const sut = new EmailValidation(faker.internet.email())    
+        const error = sut.validate(faker.internet.email())
+        expect(error).toBeFalsy();
     });
 });
