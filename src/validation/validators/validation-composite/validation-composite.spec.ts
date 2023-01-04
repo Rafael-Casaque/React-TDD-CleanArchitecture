13e4aa6257e2ac93da6fefc6bdc8d860e38dd375
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-01-03 21:07:03 -0300

    test: ensure ValidationComposite returns first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -3,18 +3,17 @@ import { FieldValidation } from "@/validation/protocols/field-validation";
 import { FieldValidationSpy } from "../test/mock-field-validation";
 import { ValidationComposite } from "./validation-composite";
 
-
-
 describe('ValidationComposite', () => {
     test('should return error if any validation fails ', () => {
         const fieldValidationSpy = new FieldValidationSpy('any_field')
+        fieldValidationSpy.error = new Error('first_error_message')
         const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-        fieldValidationSpy2.error = new Error('any_message')
+        fieldValidationSpy2.error = new Error('second_error_message')
         const sut = new ValidationComposite([
             fieldValidationSpy,
             fieldValidationSpy2
         ])
         const error = sut.validate('any_field','1')
-        expect(error).toBe('any_message')
+        expect(error).toBe('first_error_message')
     });
 });
